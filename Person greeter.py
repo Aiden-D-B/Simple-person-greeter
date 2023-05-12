82c9dc6f1b69e5746647d037dfa5febc100cec86
Aiden B <133374942+VeganMessiah@users.noreply.github.com>
2023-05-12 15:33:47 +0100

    Add files via upload

---
+++ Person greeter.py
@@ -0,0 +1,7 @@
+name = input("Hello stranger, what is your name?:")
+print(name)
+print(name,"is such a cool name! Nice to meet you!")
+fcolour = input("So what's your favourite colour:")
+print(fcolour)
+print("No way",name,"the colour",fcolour,"is my favroute colour to!")
+
